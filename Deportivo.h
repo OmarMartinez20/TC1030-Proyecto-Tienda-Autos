7d46c498106d74dd2b0190a9c64936d54e8ed89a
OmarMartinez20 <117236144+OmarMartinez20@users.noreply.github.com>
2023-06-16 07:39:12 -0600

    Update Deportivo.h

--- Deportivo.h
+++ Deportivo.h
@@ -1,10 +1,11 @@
 /*
-*  Proyecto Tienda_superautos
-*  Deportivo.h
-*  c++
-*  A01710263
-*  Created by José Omar Martínez on 29/05/23.
-*/
+ *  Proyecto Tienda_superautos
+ *  Deportivo.h
+ *  c++
+ *  A01710263
+ *  Created by José Omar Martínez on 29/05/23.
+ *  Calse Dportivo hija de clase Vehiculo
+ */
 
 #ifndef Deportivo_h
 #define Deportivo_h
@@ -25,7 +26,6 @@ public:
         aceleracion_max = aceleracion;
     }
     int getAceleracion();
-    void setAceleracion(int aceleracion);
     string to_string();
 };
 
@@ -37,28 +37,20 @@ int Deportivo::getAceleracion(){
     return aceleracion_max;
 }
 
-/**
- * Setter para modificar la aceleración máxima
- *
- */
-void Deportivo::setAceleracion(int aceleracion){
-    aceleracion_max = aceleracion;
-}
-
-
 
 /**
  * to_string convierte a atributos a string.
  *
  * concatena todos los valores de los atributos en un string para ser impreso
  *
+ * param
+ * return string con los valores y texto concatenado.
  */
 string Deportivo::to_string(){
     stringstream aux;
-    aux << " Su vehículo personalizado tiene las siguientes caracterísitcas: " << " año: " << año << "\n " << " marca: " << marca << "\n " << " potencia " << potencia << "\n " << " color " << color << "\n " << " aceleracion " << aceleracion_max << "\n ";
+    aux << " Su vehículo personalizado tiene las siguientes caracterísitcas: " << "\n " << " año: " << año << "\n " << " marca: " << marca << "\n " << " potencia " << potencia << "\n " << " color " << color << "\n " << " aceleracion " << aceleracion_max << "\n ";
     return aux.str();
 }
 
 
 #endif /* Deportivo_h */
-
